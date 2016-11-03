ce6e9462a460a3bb8a240f24a7b3c64ad2a47ae0
Ken Kubiak <ken@mnmlst.me>
2016-11-03 07:18:16 -0700

    draw a resizable SVG circle

--- src/elm/Components/Hello.elm
+++ src/elm/Components/Hello.elm
@@ -1,12 +1,25 @@
 module Components.Hello exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, h1, text)
+-- import Html.Attributes exposing ()
+import Svg exposing (svg, g, circle)
+import Svg.Attributes exposing (r, height, width, transform)
+
 import String
 
 -- hello component
 hello : Int -> Html a
 hello model =
-  div
-    [ class "h1" ]
-    [ text ( "Hello, Elm" ++ ( "!" |> String.repeat model ) ) ]
+    let radius = model + 10
+    in
+        div [] [
+            h1 [] [
+                text ( "Hello, Elm" ++ ( "!" |> String.repeat model ) )
+            ]
+            , svg [height (toString (2 * radius) ++ "px"),
+                    width (toString (2 * radius) ++ "px")] [
+                g [transform ("translate(" ++ toString radius ++ "," ++ toString radius ++ ")")] [
+                    circle [r (toString radius)] []
+                ]
+            ]
+        ]
