b026212eecc1ade1274354a34c4fd179a1959cee
Ken Kubiak <ken@mnmlst.me>
2016-12-02 07:54:56 -0800

    move content to main program, pass as List (Html children)

--- src/elm/Components/Enclave.elm
+++ src/elm/Components/Enclave.elm
@@ -6,9 +6,9 @@ import Svg exposing (svg, g, circle)
 import Svg.Attributes exposing (r, height, width, transform)
 
 -- enclave component—flow content into a bounded area
-enclave : Int -> Html a
-enclave model =
-    let radius = model + 10
+enclave : Int -> List (Html msg) -> Html msg
+enclave param content =
+    let radius = param + 10
         rs = toString radius
         ds = toString (2 * radius)
         dim = ds ++ "px"
@@ -42,48 +42,5 @@ enclave model =
             , div [class "textShape-right",
                     style[("shapeOutside", rightShape)]] []
             , bg
-            , section [class "flow"] [
-                p [class "stanza"] [
-                    span [class "line"] [ text "`Twas brillig, and the slithy toves" ]
-                    , span [class "line"] [ text "  Did gyre and gimble in the wabe:" ]
-                    , span [class "line"] [ text "All mimsy were the borogoves," ]
-                    , span [class "line"] [ text "  And the mome raths outgrabe." ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "\"Beware the Jabberwock, my son!" ]
-                    , span [class "line"] [ text "  The jaws that bite, the claws that catch!" ]
-                    , span [class "line"] [ text "Beware the Jubjub bird, and shun" ]
-                    , span [class "line"] [ text "  The frumious Bandersnatch!\"" ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "He took his vorpal sword in hand:" ]
-                    , span [class "line"] [ text "  Long time the manxome foe he sought --" ]
-                    , span [class "line"] [ text "So rested he by the Tumtum tree," ]
-                    , span [class "line"] [ text "  And stood awhile in thought." ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "And, as in uffish thought he stood," ]
-                    , span [class "line"] [ text "  The Jabberwock, with eyes of flame," ]
-                    , span [class "line"] [ text "Came whiffling through the tulgey wood," ]
-                    , span [class "line"] [ text "  And burbled as it came!" ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "One, two! One, two! And through and through" ]
-                    , span [class "line"] [ text "  The vorpal blade went snicker-snack!" ]
-                    , span [class "line"] [ text "He left it dead, and with its head" ]
-                    , span [class "line"] [ text "  He went galumphing back." ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "\"And, has thou slain the Jabberwock?" ]
-                    , span [class "line"] [ text "  Come to my arms, my beamish boy!" ]
-                    , span [class "line"] [ text "O frabjous day! Callooh! Callay!\"" ]
-                    , span [class "line"] [ text "  He chortled in his joy." ]
-                ]
-                , p [class "stanza"] [
-                    span [class "line"] [ text "`Twas brillig, and the slithy toves" ]
-                    , span [class "line"] [ text "  Did gyre and gimble in the wabe;" ]
-                    , span [class "line"] [ text "All mimsy were the borogoves," ]
-                    , span [class "line"] [ text "  And the mome raths outgrabe." ]
-                ]
-            ]
+            , section [class "flow"] content
         ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -34,10 +34,56 @@ update msg model =
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+content: List (Html msg)
+content = [
+    p [class "stanza"] [
+        span [class "line"] [ text "`Twas brillig, and the slithy toves" ]
+        , span [class "line"] [ text "  Did gyre and gimble in the wabe:" ]
+        , span [class "line"] [ text "All mimsy were the borogoves," ]
+        , span [class "line"] [ text "  And the mome raths outgrabe." ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "\"Beware the Jabberwock, my son!" ]
+        , span [class "line"] [ text "  The jaws that bite, the claws that catch!" ]
+        , span [class "line"] [ text "Beware the Jubjub bird, and shun" ]
+        , span [class "line"] [ text "  The frumious Bandersnatch!\"" ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "He took his vorpal sword in hand:" ]
+        , span [class "line"] [ text "  Long time the manxome foe he sought --" ]
+        , span [class "line"] [ text "So rested he by the Tumtum tree," ]
+        , span [class "line"] [ text "  And stood awhile in thought." ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "And, as in uffish thought he stood," ]
+        , span [class "line"] [ text "  The Jabberwock, with eyes of flame," ]
+        , span [class "line"] [ text "Came whiffling through the tulgey wood," ]
+        , span [class "line"] [ text "  And burbled as it came!" ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "One, two! One, two! And through and through" ]
+        , span [class "line"] [ text "  The vorpal blade went snicker-snack!" ]
+        , span [class "line"] [ text "He left it dead, and with its head" ]
+        , span [class "line"] [ text "  He went galumphing back." ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "\"And, has thou slain the Jabberwock?" ]
+        , span [class "line"] [ text "  Come to my arms, my beamish boy!" ]
+        , span [class "line"] [ text "O frabjous day! Callooh! Callay!\"" ]
+        , span [class "line"] [ text "  He chortled in his joy." ]
+    ]
+    , p [class "stanza"] [
+        span [class "line"] [ text "`Twas brillig, and the slithy toves" ]
+        , span [class "line"] [ text "  Did gyre and gimble in the wabe;" ]
+        , span [class "line"] [ text "All mimsy were the borogoves," ]
+        , span [class "line"] [ text "  And the mome raths outgrabe." ]
+    ]]
+
+
 view : Model -> Html Msg
 view model =
     div [class "container"] [
-        enclave model
+        enclave model content
         , button [onClick Increment] [text "Larger"]
         , button [onClick Reset] [text "Reset"]
     ]
